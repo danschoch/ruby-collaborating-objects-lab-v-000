40ebf57d9fce7a28f16cd8da60a9810c90412e8a
Daniel Schoch <d.schoch90@gmail.com>
2018-08-26 18:02:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,6 @@ class Song
   def self.new_by_filename(filename)
     file_array = filename.split(" - ")
     new_song = self.new(file_array[1])
-    new_song.artist_name = file_array[0]
-    binding.pry
+    new_song.artist = file_array[0]
   end
 end
