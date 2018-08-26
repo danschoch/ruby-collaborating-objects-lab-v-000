33bc84fa60e42f88fb00cdd46e5f26e1c7de0816
Daniel Schoch <d.schoch90@gmail.com>
2018-08-26 17:57:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,7 @@ class Song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
+    self.artist = Artist.find_or_create_by_name(name)
   end
 
   def self.new_by_filename(filename)
